b8c2b15fef8ea8f5a8aba21128bdb87be8ab49b4
Jemish-Tejani <tejanijemis@gmail.com>
2025-05-23 22:56:22 +0530

    80% work done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60, 
+  loop: true,
+});
