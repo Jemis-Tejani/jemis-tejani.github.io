6424df8943140b4721d61d5c4db7218d3160e3ba
Jemish-Tejani <tejanijemis@gmail.com>
2025-05-24 15:22:25 +0530

    A function has been added that automatically changes age over time.

--- js/script.js
+++ js/script.js
@@ -85,3 +85,20 @@ const navTogglerBtn = document.querySelector(".nav-toggler"),
           allSectione[i].classList.toggle("open");
         }
       }
+
+      function calculateAge(birthDateString) {
+    const today = new Date();
+    const birthDate = new Date(birthDateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+// Your birthdate is 2003-10-04
+const userAge = calculateAge("2003-10-04");
+document.getElementById("user-age").textContent = userAge;
